f7c443de11d05769d7d44f4657c1ed810e312ef6
opticaldrive <138826219+opticaldrive@users.noreply.github.com>
2025-08-20 15:20:48 -0700

    aiohttp????
    I have no idea how to use asyncio properly.
    Time to learn how, yay!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import time
+import aiohttp
+import asyncio
 
 
 def get_tokens_from_file(filepath: str):
@@ -13,7 +15,7 @@ def get_tokens_from_file(filepath: str):
     return tokens
 
 
-def get_thread_message_count(token):
+async def get_thread_message_count(session, token):
     """GET https://slack.com/api/conversations.history
     Authorization: Bearer xoxb-your-token
     {
@@ -23,8 +25,8 @@ def get_thread_message_count(token):
     "inclusive": true
     }
     """
-
     url = "https://slack.com/api/conversations.history"
+
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}",
@@ -38,16 +40,19 @@ def get_thread_message_count(token):
         "inclusive": "true",
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    return response
+    # response = requests.post(url, headers=headers, data=json.dumps(payload))
+    async with session.post(url, headers=headers, json=payload) as response:
+        return await response.json()
 
 
-def send_message(
+async def send_message(
+    session,
     token: str,
     channel: str,
     text: str,
     thread_ts=None,
 ):
+
     url = "https://slack.com/api/chat.postMessage"
     headers = {
         "Content-Type": "application/json",
@@ -63,22 +68,38 @@ def send_message(
     if thread_ts is not None:
         payload["thread_ts"] = thread_ts
 
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # response = requests.post(url, headers=headers, data=json.dumps(payload))
+    async with session.post(url, headers=headers, json=payload) as response:
+        return await response.json()
 
 
-def send_threadgrower_message(token):
-    send_message(token, channel="C06QV2T1P4G", text="a", thread_ts="1710818631.730789")
+async def send_threadgrower_message(session, token):
+    return await send_message(
+        session, token, channel="C06QV2T1P4G", text="a", thread_ts="1710818631.730789"
+    )
 
 
 tokens = get_tokens_from_file("tokens.txt")
 token = tokens[0]
 
-# count_response = get_thread_message_count(token)
-# count = count_response.json()
-# print(count)
-# print(count["messages"][0]["reply_count"])
 
-# while True:
-#     loop_start = time.time()
-#     send_threadgrower_message(token=token)
-#     print(time.time() - loop_start)
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        count = await get_thread_message_count(session, token)
+        print(count["messages"][0]["reply_count"])
+
+        while True:
+
+            loop_start = time.time()
+            send_json = await send_threadgrower_message(session, token=token)
+            send_ok = send_json["ok"]
+            print(send_ok)
+            # if send_ok == False:
+            #     print(send_json)
+            #     while True:
+            #         None
+            print(time.time() - loop_start)
+
+
+asyncio.run(main())
